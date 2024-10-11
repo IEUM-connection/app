7bf268aae89dab6bd1f9de5afe54b984cbdf6f7c
wooil6 <wooilkf@naver.com>
2024-10-11 12:38:22 +0900

    feat: #32 자이로센서 알람

--- android/app/src/main/java/com/ieum/MainActivity.kt
+++ android/app/src/main/java/com/ieum/MainActivity.kt
@@ -18,20 +18,19 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
-
-// MainActivity 클래스 선언. ReactActivity를 상속받음
 class MainActivity : ReactActivity() {
 
-    // gyroSensorManager 변수 선언
-    private lateinit var gyroSensorManager: GyroSensorManager
-
-    // 액티비티가 처음 생성될 때 호출되는 메서드
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("MainActivity", "MainActivity onCreate 호출됨")
 
         // GyroSensorService 시작
         Intent(this, GyroSensorService::class.java).also { intent ->
-            startService(intent)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(intent)
+            } else {
+                startService(intent)
+            }
         }
 
         // 배터리 최적화 비활성화
@@ -41,26 +40,26 @@ class MainActivity : ReactActivity() {
         checkPermissions()
     }
 
-    // 권한 요청 메서드
     private fun checkPermissions() {
-        val bodySensorsPermissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.BODY_SENSORS)
-        val activityRecognitionPermissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION)
-
         val permissionsNeeded = mutableListOf<String>()
 
-        if (bodySensorsPermissionCheck != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BODY_SENSORS) != PackageManager.PERMISSION_GRANTED) {
             permissionsNeeded.add(Manifest.permission.BODY_SENSORS)
         }
 
-        if (activityRecognitionPermissionCheck != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED) {
             permissionsNeeded.add(Manifest.permission.ACTIVITY_RECOGNITION)
+        }
+
+        // Android 13 이상에서 POST_NOTIFICATIONS 권한 추가 요청
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                permissionsNeeded.add(Manifest.permission.POST_NOTIFICATIONS)
+            }
         }
 
         if (permissionsNeeded.isNotEmpty()) {
             ActivityCompat.requestPermissions(this, permissionsNeeded.toTypedArray(), 1)
-        } else {
-            // 권한이 이미 승인된 경우 gyroSensorManager 초기화
-            initializeGyroSensorManager()
         }
     }
 
@@ -68,25 +67,11 @@ class MainActivity : ReactActivity() {
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == 1) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Log.d("Permission", "센서 권한이 승인되었습니다.")
-                // 권한이 승인되면 gyroSensorManager 초기화
-                initializeGyroSensorManager()
+            if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                Log.d("Permission", "필요한 모든 권한이 승인되었습니다.")
             } else {
-                Log.d("Permission", "센서 권한이 거부되었습니다.")
-                Toast.makeText(this, "센서 권한이 필요합니다.", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    // GyroSensorManager 초기화 메서드
-    private fun initializeGyroSensorManager() {
-        gyroSensorManager = GyroSensorManager(this).apply {
-            onShockDetected = {
-                Log.d("GyroSensor", "충격 감지됨!")
-            }
-            onFallDetected = {
-                Log.d("GyroSensor", "넘어짐 감지됨!")
+                Log.d("Permission", "필요한 권한 중 일부가 거부되었습니다.")
+                Toast.makeText(this, "필요한 권한이 승인되지 않았습니다.", Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -103,12 +88,6 @@ class MainActivity : ReactActivity() {
                 startActivity(intent)
             }
         }
-    }
-
-    // 액티비티가 일시 정지될 때 호출되는 메서드
-    override fun onPause() {
-        super.onPause()
-        gyroSensorManager.unregister()
     }
 
     // 메인 컴포넌트의 이름을 반환하는 메서드
