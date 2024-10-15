0e5901ba7ee7284d21de87a3ab1372e6d65aa385
kimchanjun <rlackswns7@gmail.com>
2024-10-16 01:05:49 +0900

    Feat: 도움요청 문자발송

--- src/components/HelpRequestModal.js
+++ src/components/HelpRequestModal.js
@@ -1,89 +1,70 @@
 import React, { useState, useEffect } from 'react';
-
-import {View, Text, TouchableOpacity, Modal, StyleSheet, Platform} from 'react-native';
-
+import { View, Text, TouchableOpacity, Modal, StyleSheet, Platform } from 'react-native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import axios from 'axios';
 import * as Keychain from 'react-native-keychain';
 import { REACT_APP_API_KEY } from '@env';
 
-
 const HelpRequestModal = ({ visible, onClose }) => {
-    
-    const [memberInfo, setMemberInfo] = useState();
-    const [name, setName] = useState();
+    const [isRequestSent, setIsRequestSent] = useState(false);
 
     const sendSms = async () => {
         try {
+            const credentials = await Keychain.getGenericPassword();
+            if (!credentials) {
+                console.error("Access Token이 비어 있습니다. SMS를 보낼 수 없습니다.");
+                return;
+            }
 
-        const credentials = await Keychain.getGenericPassword(); // AccessToken을 가져옵니다.
-        if (!credentials) {
-            console.error("Access Token이 비어 있습니다. SMS를 보낼 수 없습니다.");
-            return;
+            const accessToken = credentials.password;
+            const memberData = await fetchMemberInfo(accessToken);
+
+            if (memberData) {
+                const guardianPh = memberData.guardianPhone.replaceAll("-", "");
+                const adminPh = memberData.adminPhone.replaceAll("-", "");
+
+                const smsBody = `${memberData.name}님이 도움을 요청하셨습니다.\n즉시 연락 바랍니다.\n -이음-`;
+
+                const smsRequest = {
+                    body: smsBody,
+                    gudianNum: guardianPh,
+                    adminNum: adminPh
+                };
+
+                const response = await axios.post(`${REACT_APP_API_KEY}/send-sms`, smsRequest);
+
+                if (response.status === 200) {
+                    setIsRequestSent(true); // 요청 성공 시 상태 변경
+                } else {
+                    const errorBody = await response.text();
+                    console.error(`SMS 전송 실패: ${response.statusText}, 응답 코드: ${response.status}, 에러 내용: ${errorBody}`);
+                }
+            } else {
+                console.error("멤버 정보를 가져오는 데 실패했습니다.");
+            }
+        } catch (error) {
+            console.error("오류 발생:", error);
         }
-
-        const accessToken = credentials.password; // JSON 파싱하지 않고 바로 토큰 사용
-        await fetchMemberInfo(accessToken);
-        
-        if (memberInfo) {
-            // guardianPhone과 adminPhone이 존재하는지 확인 후 처리
-            console.log("회원 정보", memberInfo);
-            console.log("보호자 연락처", memberInfo.guardianPhone);
-            console.log("관리자 연락처", memberInfo.adminPhone);
-            const guardianPh = memberInfo.guardianPhone.replaceAll("-", "") ;
-            const adminPh = memberInfo.adminPhone.replaceAll("-", "");
-        
-
-        const smsBody =  `${memberInfo.name}님이 도움을 요청하셨습니다.\n즉시 연락 바랍니다.\n -이음-`;
-
-        const smsRequest = {
-            body : smsBody,
-            gudianNum:guardianPh,
-            adminNum: adminPh
-        }
-
-        const smsData = JSON.stringify(smsRequest);
-        console.log("sms 전송 데이터", smsData);
-
-
-        const response = await axios.post(`${REACT_APP_API_KEY}/send-sms`, 
-            smsRequest,
-            
-        );
-
-        if (response.status === 200) { // 응답이 성공적인지 확인
-            // const responseBody = await response.json();
-            // console.log("SMS 전송 성공:", responseBody); // 성공 로그 출력
-        } else { // 응답이 실패한 경우
-            const errorBody = await response.text(); // 에러 본문 가져오기
-            console.error(`SMS 전송 실패: ${response.statusText}, 응답 코드: ${response.status}, 에러 내용: ${errorBody}`); // 실패 로그 출력
-        }
-        } else {
-            console.error("멤버 정보를 가져오는 데 실패했습니다.");
-        }
-    } catch (error) {
-        console.error("오류 발생:", error);
-       }
-    }
-
-
+    };
 
     const fetchMemberInfo = async (accessToken) => {
         try {
             const memberResponse = await axios.get(`${REACT_APP_API_KEY}/members/member`, {
                 headers: {
-                    'Authorization': `Bearer ${accessToken}` // Bearer 형식으로 토큰 추가
+                    'Authorization': `Bearer ${accessToken}`
                 }
             });
-    
-            console.log('memberInfo', memberResponse.data.data);
-            setMemberInfo(memberResponse.data.data); // 상태 업데이트
-            return memberResponse.data.data; // 멤버 정보 반환
-    
+
+            return memberResponse.data.data;
         } catch (error) {
             console.error('사용자 정보를 가져오는데 실패했습니다', error);
-            return null; // 실패 시 null 반환
+            return null;
         }
+    };
+
+    const handleClose = () => {
+        setIsRequestSent(false);
+        onClose();
     };
 
     return (
@@ -91,20 +72,26 @@ const HelpRequestModal = ({ visible, onClose }) => {
             visible={visible}
             animationType="fade"
             transparent={true}
-            onRequestClose={onClose}
+            onRequestClose={handleClose}
         >
             <View style={styles.modalBackground}>
                 <View style={[styles.modalContainer, styles.shadowProp]}>
-                    <TouchableOpacity style={styles.closeButton} onPress={onClose}>
+                    <TouchableOpacity style={styles.closeButton} onPress={handleClose}>
                         <AntDesign name="close" size={40} color="#909090" />
                     </TouchableOpacity>
-                    <Text style={[styles.modalText,{marginTop: 60}]}>도움을 </Text>
-                    <Text style={[styles.modalText,{marginBottom: 30}]}>요청하시겠습니까?</Text>
-                    {/* 요청하기 버튼 */}
-                    <TouchableOpacity style={[styles.requestButton,styles.shadowProp]} onPress={sendSms}>
-
-                        <Text style={styles.requestButtonText}>요청하기</Text>
-                    </TouchableOpacity>
+                    <View style={styles.contentWrapper}>
+                        {isRequestSent ? (
+                            <Text style={[styles.modalText, { marginTop: 10 }]}>도움이 요청되었습니다</Text>
+                        ) : (
+                            <>
+                                <Text style={[styles.modalText, { marginTop: 60 }]}>도움을</Text>
+                                <Text style={[styles.modalText, { marginBottom: 30 }]}>요청하시겠습니까?</Text>
+                                <TouchableOpacity style={[styles.requestButton, styles.shadowProp]} onPress={sendSms}>
+                                    <Text style={styles.requestButtonText}>요청하기</Text>
+                                </TouchableOpacity>
+                            </>
+                        )}
+                    </View>
                 </View>
             </View>
         </Modal>
@@ -135,6 +122,12 @@ const styles = StyleSheet.create({
         right: 25,
         marginBottom: 10,
     },
+    contentWrapper: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 300, // 모달 내용 크기를 고정해서 버튼과 텍스트 배치가 동일하게 유지되도록 설정
+    },
     modalText: {
         fontSize: 35,
         textAlign: 'center',
@@ -148,7 +141,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         width: '60%',
-        height: 70,
+        height: 70, // 기존 버튼 크기 유지
         alignSelf: 'center',
         marginBottom: 60,
     },
